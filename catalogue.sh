27b5c566abfd21d7ce92f89dd491a6e268c56497
sundar <sundarsodadasi1996@gmail.com>
2025-02-07 04:58:40 +0530

    First commit

--- catalogue.sh
+++ catalogue.sh
@@ -60,7 +60,7 @@ npm install &>>$LOGFILE
 VALIDATE $? "Installing dependencies"
 
 # give full path of catalogue.service because we are inside /app
-cp /home/centos/current-project/catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
+cp /home/centos/currect-project/catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
 
 VALIDATE $? "copying catalogue.service"
 
@@ -76,7 +76,7 @@ systemctl start catalogue &>>$LOGFILE
 
 VALIDATE $? "Starting Catalogue"
 
-cp /home/centos/current-project/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
+cp /home/centos/currect-project/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
 
 VALIDATE $? "Copying mongo repo"
 
