5ba4ad87a965b7f7aa22e26a98677316f45143d5
sundar <sundarsodadasi1996@gmail.com>
2025-02-21 14:43:36 +0530

    First commit

--- 01-create-ec2-route53.sh
+++ 01-create-ec2-route53.sh
@@ -8,38 +8,40 @@ DOMAIN_NAME=sadhusundar.xyz
 
 # if mysql or mongodb or shipping instance_type should be t3.medium , for all others it is t2.micro
 
-for i in "${NAMES[@]}"
-do  
-    if [[ $i == "mongodb" || $i == "mysql" || $i == "shipping" ]]
-    then
+for i in "${NAMES[@]}"; do  
+    if [[ $i == "mongodb" || $i == "mysql" || $i == "shipping" ]]; then
         INSTANCE_TYPE="t3.medium"
     else
         INSTANCE_TYPE="t2.micro"
     fi
-    echo "creating $i instance"
+    
+    echo "Creating $i instance"
+    
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
-    echo "created $i instance: $IP_ADDRESS"
 
-    for i in service1 service2 web service3; do
-    if [[ "$i" == *"web"* ]]; then
-        VISIBILITY="Public"
-    else
-        VISIBILITY="Private"
-    fi
+    echo "Created $i instance: $IP_ADDRESS"
 
-    echo "Creating Route 53 record for $i with $VISIBILITY visibility"
+    # Now creating Route 53 records
+    for j in service1 service2 web service3; do  # Changed loop variable to 'j'
+        if [[ "$j" == *"web"* ]]; then
+            VISIBILITY="Public"
+        else
+            VISIBILITY="Private"
+        fi
 
-    aws route53 change-resource-record-sets --hosted-zone-id Z0824164232GERUT9YSUC --change-batch '
-    {
-            "Changes": [{
-            "Action": "CREATE",
-                        "ResourceRecordSet": {
-                            "Name": "'$i.$DOMAIN_NAME'",
-                            "Type": "A",
-                            "TTL": 1,
-                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
-                        }}]
-    }
-    '
-done
+        echo "Creating Route 53 record for $j with $VISIBILITY visibility"
 
+        aws route53 change-resource-record-sets --hosted-zone-id Z0824164232GERUT9YSUC --change-batch '
+        {
+                "Changes": [{
+                "Action": "CREATE",
+                            "ResourceRecordSet": {
+                                "Name": "'$j.$DOMAIN_NAME'",
+                                "Type": "A",
+                                "TTL": 1,
+                                "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                            }}]
+        }
+        '
+    done
+done  # Closing the first loop properly
